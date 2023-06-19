d8b4e45ad3e3d4b42222bd20a3f0caff612b8c23
andrewkusakin <akusaki@wgu.edu>
2023-06-19 12:57:59 -0400

    added model to edit gallery

--- src/main/UI/src/components/galleryItem/GalleryItem.tsx
+++ src/main/UI/src/components/galleryItem/GalleryItem.tsx
@@ -1,25 +1,44 @@
-import React, { useState } from 'react'
-import './galleryItem.css'
-import { Photo } from "./../../pages/dashboard/Dashboard"
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./galleryItem.css";
+import { Photo } from "./../../pages/dashboard/Dashboard";
+import { Link } from "react-router-dom";
+import ModeEditIcon from "@mui/icons-material/ModeEdit";
+import IosShareIcon from "@mui/icons-material/IosShare";
 
-interface GalleryItemProps{
-    photo: Photo;
+interface GalleryItemProps {
+  photo: Photo;
 }
 
 export default function GalleryItem(props: GalleryItemProps) {
   const { photo } = props;
 
   const [btnClicked, setButtonClicked] = useState(false);
-  
+
   return (
-    <div className='gallery-item'>
+    <div className="gallery-item">
       <div onClick={() => setButtonClicked(true)}>
         <img src={photo.url} alt="" />
       </div>
-      <p>Gallery Name</p>
-      <p>FEB 20 2023</p>
-      {btnClicked ? <div className='edit-gallery'><button onClick={() => setButtonClicked(false)}>Close</button></div> : <></>}
+      <div className="gallery-item-flexbox">
+        <div > 
+          <p>Gallery Name</p>
+          <p>FEB 20 2023</p>
+        </div>
+       
+        <div className="gallery-item-icons">
+          <Link to="./editgallery" className="gallery-item-icons"><ModeEditIcon /></Link>
+          <Link to="./sharegallery" className="gallery-item-icons"><IosShareIcon /></Link>
+          
+        </div>
+      </div>
+
+      {btnClicked ? (
+        <div className="edit-gallery">
+          <button onClick={() => setButtonClicked(false)}>Close</button>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
