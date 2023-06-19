a868729356ad21c79265772a1151940aefb9d631
andrewkusakin <akusaki@wgu.edu>
2023-06-19 13:00:14 -0400

    updated post request

--- src/main/UI/src/components/signUp/SignUp.tsx
+++ src/main/UI/src/components/signUp/SignUp.tsx
@@ -1,46 +1,78 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './signUp.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./signUp.css";
+import { Link } from "react-router-dom";
+import CloseIcon from "@mui/icons-material/Close";
 
-export default function SignUp(){
-  const [businessName, setBusinessName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+export default function SignUp() {
+  const [businessName, setBusinessName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = (event: { preventDefault: () => void; }) => {
+  const handleSubmit = (event: { preventDefault: () => void }) => {
     event.preventDefault();
 
     const user = {
       businessName: businessName,
       email: email,
-      password: password
+      password: password,
     };
 
-    axios.post(`/users`, user)
-    //   .then(res => {
-    //     console.log(res);
-    //     console.log(res.data);
-    //   })
-      .catch(error => console.error(error));
+    axios
+      .post(`/users`, user)
+        .then(res => {
+          console.log(res);
+          console.log(res.data);
+          window.location.href = "/login";
+        })
+      .catch((error) => console.error(error));
   };
 
-  return (<div className='sign-up-wrapper'>sign up
-    <form onSubmit={handleSubmit}>
-      <label>
-        Business Name:
-        <input type="text" value={businessName} onChange={e => setBusinessName(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  </div>
-    
+  return (
+    <div className="sign-up-wrapper">
+      <div className="sign-up-window">
+        <Link to="/" className="login-close">
+          <CloseIcon fontSize="large" />
+        </Link>
+        <h2 className="login-title">Create your new account</h2>
+          <p className="login-subtitle">
+          Already have an account?{" "}
+            <Link to="/Login" className="subtitle-signup-link">
+              Log in
+            </Link>
+          </p>
+        <form onSubmit={handleSubmit} className="login-form">
+          <input
+            className="input-field"
+            placeholder="Business Name"
+            type="text"
+            value={businessName}
+            onChange={(e) => setBusinessName(e.target.value)}
+          />
+
+          <input
+            className="input-field"
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+
+          <input
+            className="input-field"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+
+          <button type="submit" className="submit-btn">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
