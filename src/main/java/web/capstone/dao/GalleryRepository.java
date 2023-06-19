0b76f07393e6ab8944b45098ac2fb415af013a3c
andrewkusakin <akusaki@wgu.edu>
2023-06-19 13:10:43 -0400

    added Query findAllByUserId

--- src/main/java/web/capstone/dao/GalleryRepository.java
+++ src/main/java/web/capstone/dao/GalleryRepository.java
@@ -1,11 +1,17 @@
 package web.capstone.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import web.capstone.entities.Gallery;
 
+import java.util.List;
+
 @CrossOrigin
 @RequestMapping("/api")
 public interface GalleryRepository extends JpaRepository<Gallery, Long> {
+
+    @Query("SELECT g FROM Gallery g WHERE g.user = ?1")
+    List<Gallery> findAllByUserId(Long userId);
 }
