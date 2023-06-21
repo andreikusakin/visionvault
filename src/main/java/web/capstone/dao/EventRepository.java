b0806d50eb76cdec7238be7a73b62206c923594f
andrewkusakin <akusaki@wgu.edu>
2023-06-21 01:10:12 -0400

    added query deleteAllByGalleryId

--- src/main/java/web/capstone/dao/EventRepository.java
+++ src/main/java/web/capstone/dao/EventRepository.java
@@ -1,6 +1,8 @@
 package web.capstone.dao;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,4 +16,9 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("SELECT e FROM Event e WHERE e.gallery.id = ?1 ORDER BY e.id ASC")
     List<Event> getAllEventsByGalleryId(Long galleryId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Event e WHERE e.gallery.id = ?1")
+    void deleteAllByGalleryId(Long galleryId);
 }
