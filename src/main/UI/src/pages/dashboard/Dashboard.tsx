7f7434e36af6811704189d3e565b55ccebe3c53f
andrewkusakin <akusaki@wgu.edu>
2023-06-21 01:09:03 -0400

    updated css and design

--- src/main/UI/src/pages/dashboard/Dashboard.tsx
+++ src/main/UI/src/pages/dashboard/Dashboard.tsx
@@ -5,13 +5,10 @@ import { Link, Outlet } from "react-router-dom";
 import axios from "axios";
 import { Gallery } from "../../components/interfaces";
 
-
-
 export default function Dashboard() {
-
   const [galleriesArray, setGalleriesArray] = useState<Gallery[]>([]);
-  const [searchTerm, setSearchTerm] = useState('');
-
+  const [searchTerm, setSearchTerm] = useState("");
+  
   const [isUserId, setIsUserId] = useState(
     localStorage.getItem("authUserId") !== null
   );
@@ -24,65 +21,71 @@ export default function Dashboard() {
   console.log(localStorage.getItem("authUserId") + "USER ID");
   console.log(localStorage.getItem("authBusinessName") + "Business Name");
 
-  useEffect(() => {axios
-    .get('/mygalleries', {
-      params: {
-        userId: localStorage.getItem("authUserId")
-      }
-    })
-    .then((res) => {
-      setGalleriesArray(res.data);
-    })
-    .catch((error) => console.error(error));}, []);
+  useEffect(() => {
+    axios
+      .get("/mygalleries", {
+        params: {
+          userId: localStorage.getItem("authUserId"),
+        },
+      })
+      .then((res) => {
+        setGalleriesArray(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div className="dashboard-wrapper">
       {isUserId ? (
-        <><div className="dashboard-menu">
-        <a href="/dashboard" className="logo-dashboard">
-          VisionVault
-        </a>
-        <div>Edit Account</div>
-        <Link to="/" onClick={signOut}>
-          Sign Out
-        </Link>
-      </div>
-      <div className="dashboard-content">
-        <Link to="./creategallery" className="btn-add-gallery">
-          Create Gallery
-        </Link>
-        <input
+        <>
+          <div className="dashboard-menu">
+            <a href="/" className="logo-dashboard">
+              VisionVault
+            </a>
+            <div className="dashboard-menu-actions"><Link to="./reports" className="dashboard-menu-btn" >Reports</Link>
+            <Link to="/" onClick={signOut} className="dashboard-menu-btn">
+              Sign Out
+            </Link>
+          </div></div>
+            
+          <div className="dashboard-content">
+            <Link to="./creategallery" className="btn-add-gallery">
+              Create Gallery
+            </Link>
+            
+            <input
               type="text"
               placeholder="Search"
-              onChange={e => setSearchTerm(e.target.value)}
+              className="search-bar"
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
-        <div className="gallaries-grid">
-        {galleriesArray
-                .filter(gallery => gallery.name.toLowerCase().includes(searchTerm.toLowerCase()))
-                .map(gallery => (
+            <div className="gallaries-grid">
+              {galleriesArray
+                .filter((gallery) =>
+                  gallery.name.toLowerCase().includes(searchTerm.toLowerCase())
+                )
+                .map((gallery) => (
                   <GalleryItem key={gallery.id} gallery={gallery} />
-                ))
-              }
-          
-        </div>
-        <Outlet />
-      </div></>
+                ))}
+            </div>
+            <Outlet />
+          </div>
+        </>
       ) : (
-        <><div className="dashboard-menu">
-        <a href="/dashboard" className="logo-dashboard">
-          VisionVault
-        </a>
-        
-      </div>
-      <div className="dashboard-content">
-        <h3>Sign In to View Your Galleries</h3>
-        <a href="/login" className="btn-add-gallery">
-          Login
-        </a>
-      </div></>
-        
+        <>
+          <div className="dashboard-menu">
+            <a href="/dashboard" className="logo-dashboard">
+              VisionVault
+            </a>
+          </div>
+          <div className="dashboard-content">
+            <h3>Sign In to View Your Galleries</h3>
+            <a href="/login" className="btn-add-gallery">
+              Login
+            </a>
+          </div>
+        </>
       )}
-      
     </div>
   );
 }
