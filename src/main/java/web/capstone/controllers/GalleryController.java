17f33c0857c23da0c50819e9207cbef3cdde2714
andrewkusakin <akusaki@wgu.edu>
2023-06-21 01:11:48 -0400

    added methods getGalleryRestrictedView and getGalleryWithPassword

--- src/main/java/web/capstone/controllers/GalleryController.java
+++ src/main/java/web/capstone/controllers/GalleryController.java
@@ -4,15 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import web.capstone.dao.EventRepository;
 import web.capstone.dao.GalleryRepository;
 import web.capstone.dao.PictureRepository;
 import web.capstone.dao.UserRepository;
+import web.capstone.entities.Event;
 import web.capstone.entities.Gallery;
 import web.capstone.entities.Picture;
 import web.capstone.entities.User;
 import web.capstone.model.request.CreateGalleryRequest;
 import web.capstone.model.request.UpdateGalleryRequest;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -24,11 +27,14 @@ public class GalleryController {
     final GalleryRepository galleryRepository;
     final PictureRepository pictureRepository;
     final UserRepository userRepository;
+    final EventRepository eventRepository;
 
-    public GalleryController(GalleryRepository galleryRepository, PictureRepository pictureRepository, UserRepository userRepository) {
+
+    public GalleryController(GalleryRepository galleryRepository, PictureRepository pictureRepository, UserRepository userRepository, EventRepository eventRepository) {
         this.galleryRepository = galleryRepository;
         this.pictureRepository = pictureRepository;
         this.userRepository = userRepository;
+        this.eventRepository = eventRepository;
     }
 
     @RequestMapping(path = "/creategallery", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
@@ -97,8 +103,43 @@ public class GalleryController {
 
     @DeleteMapping("/gallery/{id}")
     public void deleteGallery(@PathVariable Long id) {
+        eventRepository.deleteAllByGalleryId(id);
         pictureRepository.deleteAllByGalleryId(id);
         galleryRepository.deleteById(id);
+
+    }
+
+    @GetMapping(path = "/guestlogin/{id}")
+    public ResponseEntity<Gallery> getGalleryRestrictedView(@PathVariable Long id) {
+        Optional<Gallery> maybeGallery = galleryRepository.findById(id);
+        if (maybeGallery.isPresent()) {
+            Gallery fullGallery = maybeGallery.get();
+            Gallery restrictedGallery = new Gallery();
+            restrictedGallery.setId(fullGallery.getId());
+            restrictedGallery.setName(fullGallery.getName());
+            List<Picture> pictures = pictureRepository.findPicturesByGalleryId(fullGallery.getId());
+            pictures.stream().findFirst().ifPresent(p -> restrictedGallery.setCoverUrl(p.getUrl()));
+            return new ResponseEntity<>(restrictedGallery, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(path = "/viewgallery/{id}")
+    public ResponseEntity<Gallery> getGalleryWithPassword(@PathVariable Long id, @RequestParam String password, @RequestParam String email) {
+        Optional<Gallery> maybeGallery = galleryRepository.findByIdAndPassword(id, password);
+        if (maybeGallery.isPresent()) {
+            Gallery gallery = maybeGallery.get();
+            Date now = new Date();
+            Event event = new Event(email, now, gallery);
+            eventRepository.save(event);
+
+            List<Picture> pictures = pictureRepository.findPicturesByGalleryId(gallery.getId());
+            gallery.setPictures(Set.copyOf(pictures));
+            return new ResponseEntity<>(gallery, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
     }
 
 }
