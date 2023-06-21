9a8ba41bd4bebcf9c069dff4d427b30d1e6eb485
andrewkusakin <akusaki@wgu.edu>
2023-06-21 01:16:13 -0400

    updated import

--- src/main/java/web/capstone/controllers/ReportController.java
+++ src/main/java/web/capstone/controllers/ReportController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import web.capstone.model.request.Report;
+import web.capstone.model.Report;
 import web.capstone.services.ReportService;
 
 import java.util.List;
